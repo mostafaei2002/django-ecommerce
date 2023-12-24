97168d6552f6d7124b561474d55dc17e397a5762
mostafaei2002 <sinamostafaei2002@gmail.com>
2023-12-24 11:37:13 +0330

    add single product route/template

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
+from .forms import RegisterForm
 from .models import Product
 
 # Create your views here.
@@ -28,3 +29,26 @@ class ProductList(ListView):
     template_name = "shop/product_list.html"
     model = Product
     context_object_name = "products_list"
+
+
+class ProductView(DetailView):
+    template_name = "shop/single_product.html"
+    model = Product
+    context_object_name = "product"
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "shop/login.html")
+
+    def post(self, request):
+        pass
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, "shop/register.html", {"form": form})
+
+    def post(self, request):
+        pass
