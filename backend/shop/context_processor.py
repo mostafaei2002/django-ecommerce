c22b15f72351badca1d6d8bb8b38c1e13c2fe516
mostafaei2002 <sinamostafaei2002@gmail.com>
2023-12-30 18:10:01 +0330

    add shopping cart offcanvas

--- backend/shop/context_processor.py
+++ backend/shop/context_processor.py
@@ -2,12 +2,13 @@ from .models import Cart
 
 
 def cart_context(request):
-    user = request.user
+    # user = request.user
 
-    if user.is_authenticated:
-        active_cart = user.carts.get(status="active")
-    else:
-        active_cart_id = request.session["active_cart_id"]
-        active_cart = Cart.objects.get(pk=active_cart_id)
+    # if user.is_authenticated:
+    #     active_cart = user.carts.get(status="active")
+    # else:
+    #     active_cart_id = request.session["active_cart_id"]
+    #     active_cart = Cart.objects.get(pk=active_cart_id)
 
-    return {"cart": active_cart}
+    # return {"cart": active_cart}
+    return {}
--- frontend/js/bootstrap.js
+++ frontend/js/bootstrap.js
@@ -1,13 +1,15 @@
 import "../scss/bootstrap.scss";
 
+// import "@popperjs/core";
+
 // import 'bootstrap/js/dist/alert';
 // import 'bootstrap/js/dist/button';
 // import 'bootstrap/js/dist/carousel';
-import "bootstrap/js/dist/collapse";
-import "bootstrap/js/dist/popover";
+// import "bootstrap/js/dist/collapse";
+// import "bootstrap/js/dist/popover";
 import "bootstrap/js/dist/dropdown";
 // import "bootstrap/js/dist/modal";
-// import 'bootstrap/js/dist/offcanvas';
+import "bootstrap/js/dist/offcanvas";
 // import 'bootstrap/js/dist/scrollspy';
 import "bootstrap/js/dist/tab";
 // import 'bootstrap/js/dist/toast';
