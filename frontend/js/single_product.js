18fd8fa3bf59a50d538cbf99fc85dfc06cdbea99
mostafaei2002 <sinamostafaei2002@gmail.com>
2023-12-30 15:04:18 +0330

    add user profile tabs

--- frontend/js/single_product.js
+++
@@ -1,9 +0,0 @@
-// const triggerTabList = document.querySelectorAll("#productTabs button");
-// triggerTabList.forEach((triggerEl) => {
-//     const tabTrigger = new bootstrap.Tab(triggerEl);
-
-//     triggerEl.addEventListener("click", (event) => {
-//         event.preventDefault();
-//         tabTrigger.show();
-//     });
-// });
