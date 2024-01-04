f40a20574f071fb97db29b6a0ae9a83ed1a32e74
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-01-04 13:41:35 +0330

    fix CI/CD

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("IS_DEVELOPMENT")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST")]
 
 
 # Application definition
