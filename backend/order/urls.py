cc089a07a0784b1f6a70c68466e806a390bce4e3
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-02-05 19:27:10 +0330

    modify templates for order app

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.OrdersView.as_view(), name="orders"),
     path("<int:id>", views.OrdersView.as_view(), name="single-order"),
+    path("payment/<int:id>", views.PaymentView.as_view(), name="payment"),
 ]
--- backend/order/views.py
+++ backend/order/views.py
@@ -3,18 +3,16 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
-from django_htmx.http import HttpResponseClientRedirect
+from django_htmx.http import HttpResponseClientRedirect, HttpResponseClientRefresh
 from shopping_cart.models import Cart
 
-from .models import Order, OrderItem
+from .models import Address, Order, OrderItem
 
 # Create your views here.
 
 
 class OrdersView(View):
     def get(self, request, id):
-        # Return Single order or All orders
-
         order_obj = Order.objects.get(pk=id)
         address_list = request.user.addresses.all()
 
@@ -44,3 +42,15 @@ class OrdersView(View):
                 request, "Please login/register before submitting your order."
             )
             return HttpResponse(status=204)
+
+
+class PaymentView(View):
+    def post(self, request, id):
+        order_obj = Order.objects.get(pk=id)
+        address_id = request.POST.get("address")
+        if order_obj.user == request.user:
+            order_obj.status = "finished"
+            order_obj.address = Address.objects.get(pk=address_id)
+            order_obj.save()
+            messages.success(request, "Payment Unimplemented. Order Submitted")
+        return HttpResponseClientRefresh()
